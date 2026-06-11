4d82d8511f1244fc9177d34905f15bc5182233af
aureliayee <applesause@gmail.com>
2026-06-10 19:39:06 -0700

    Assignment 1

---
+++ js/script.js
@@ -0,0 +1,108 @@
+// STEP 1
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+// STEP 2
+// 3.4
+// Aurelia
+// true
+// null
+
+// STEP 3
+// let combinedAge = 1 + 7 + 10
+// let timeToRetirement = 65 - 36
+
+//STEP 4
+// let firstName
+// let lastName
+// let strAddress
+// let strCity
+// let strState
+// let zipCode
+// let yourAge
+// let referralSource
+// let contactYou
+
+//STEP 5
+// let firstName = 'Aurelia'
+// let lastName = 'Yee'
+// let strCity = 'San Diego'
+
+//STEP 6
+// let myAge = '21' + ' years old'
+// console.log(myAge)
+
+//STEP 7
+// let firstVar = true + ' story'
+// let secondVar = 1 + false
+// console.log(firstVar)
+// console.log(secondVar)
+
+//STEP 8
+//no, you cannot use ' to open the string and also have one in the middle of the string, use \
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+//STEP 9
+// let empty = null
+// console.log(empty)
+
+// let notDefined
+// console.log(notDefined)
+
+//STEP 10
+// console.log(typeof 'String')
+// console.log(typeof 234)
+// console.log(typeof false)
+// console.log(typeof Object())
+// console.log(typeof notDefined)
+
+//STEP 11
+// alert('Hello' + ' Aurelia Yee' +', welcome to the JavaScript class!')
+
+//STEP 12
+// let name = 'Aurelia Yee'
+// alert('Hello ' + name +', welcome to the JavaScript class!')
+
+//STEP 13
+// let name = 'Aurelia Yee'
+// let course = 'JavaScript'
+// alert('Hello ' + name +', welcome to the ' + course + ' class!')
+
+//STEP 14
+// let name = 'Aurelia Yee'
+// let course = 'JavaScript'
+// alert('Hello ' + name +'.\nWelcome to the ' + course + ' class!')
+
+//STEP 15
+// let name = prompt('What is your name?')
+// let course = 'JavaScript'
+// alert('Hello ' + name +'.\nWelcome to the ' + course + ' class!')
+
+//STEP 16
+// let name = 'Aurelia'
+// let course = prompt('What class are you taking?')
+// alert('Hello ' + name +'.\nWelcome to the ' + course + ' class!')
+
+//STEP 17
+// let x = 10
+// let y = 20
+// console.log (x + y)
+
+//STEP 18
+// let x = 20
+// x += 20
+// console.log(x)
+
+//STEP 19
+// let x = 20
+// x *= 5
+// console.log(x)
+
+//STEP 20
+// let x = 20 % 3
+// x /= 1
+// console.log(x)
